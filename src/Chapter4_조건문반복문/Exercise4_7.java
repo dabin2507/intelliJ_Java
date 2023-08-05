efc6de4d571b987ab99a510841374c74ae0687bf
이다빈 <dabin2807@gmail.com>
2023-08-05 21:52:19 +0900

    자바의 정석 연습문제 Chapter4 조건문과 반복문 6~7

---
+++ src/Chapter4_조건문반복문/Exercise4_7.java
@@ -0,0 +1,11 @@
+package Chapter4_조건문반복문;
+
+public class Exercise4_7 {
+    public static void main(String[] args) {
+        int value = (int) (Math.random() * 6) + 1;
+
+        System.out.println("value: " + value);
+    }
+}
+//Math.random()을 사용하여 1부터 6사이의 임의의 정수를 변수 value에 저장하는 코드를 완성하라.
+//Math.random() 함수는 double형으로 0.0이상 1.0미만 사이의 값을 반환하는 함수다.
